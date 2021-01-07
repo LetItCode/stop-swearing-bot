5ab87d23b7b0cf53f30107b98965ef4e2f8b63c5
Josh Dies <LetItCode@users.noreply.github.com>
2021-01-07 20:48:55 +0300

    forgotten substring check

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ const { Telegraf, groupChat } = require('telegraf')
 const curseWords = ['пиздец', 'хуй', 'хуита', 'пидор', 'пидар', 'пи3дец']
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.hears(curseWords, groupChat(ctx => ctx.deleteMessage()))
+bot.hears(curseWords.map(w => new RegExp(w)), groupChat(ctx => ctx.deleteMessage()))
 bot.catch(console.trace)
 bot.launch()
